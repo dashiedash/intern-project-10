466efa094ab7f8a33340f30f45ef16d6d6922d26
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-04 16:59:34 +0800

    Trying to create a modal and failing at it
    I love PHP

--- create.php
+++ create.php
@@ -1,4 +1,6 @@
 <?php
+// create.php
+
 include 'connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -11,14 +13,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $timestamp = date('Y-m-d H:i:s');
 
   // Insert the new item into the database with created-at and updated-at values
-  $query = 'INSERT INTO `stock-system` (brand, `item-name`, `amount`, `created-at`, `updated-at`) VALUES (?, ?, ?, ?, ?)';
+  $query = 'INSERT INTO `stock-system` (brand, `item-name`, amount, `created-at`, `updated-at`) VALUES (?, ?, ?, ?, ?)';
   $stmt = $pdo->prepare($query);
   $stmt->execute([$brand, $itemName, $amount, $timestamp, $timestamp]);
 
-  header('Location: create.php');
-  exit();
+  $submissionStatus = $stmt ? 'Item added to stock.' : 'Item failed';
+  echo "<script>openModal('statusModal', '$submissionStatus');</script>";
 }
 
+// Retrieve unique brand values from the database
+$query = 'SELECT DISTINCT brand FROM `stock-system`';
+$stmt = $pdo->query($query);
+$brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
 ?>
 
 <!DOCTYPE html>
@@ -29,13 +35,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Add Item</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+  <script>
+    function confirmSubmit() {
+      openModal('confirmationModal');
+      return false;
+    }
+
+    function openModal(modalId, statusText) {
+      const modal = document.getElementById(modalId);
+      modal.classList.remove('hidden');
+      modal.classList.add('flex');
+      document.getElementById('statusModalDescription').textContent = statusText;
+    }
+
+    function closeModal() {
+      const modals = document.querySelectorAll('.modal');
+      modals.forEach(modal => {
+        modal.classList.remove('flex');
+        modal.classList.add('hidden');
+      });
+    }
+  </script>
 </head>
 
 <body class="bg-gray-100">
   <div class="container mx-auto p-4">
     <a href="index.php" class="mb-4 rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600">Back</a>
 
-    <form class="mb-4" method="POST" action="create.php">
+    <form class="mb-4" method="POST" action="create.php" onsubmit="return confirmSubmit();">
       <label for="brand" class="block mb-2 font-bold">Brand:</label>
       <input type="text" id="brand" name="brand" class="w-full p-2 mb-4" list="brandList" required>
       <datalist id="brandList">
@@ -55,6 +83,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
       <button type="submit" class="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600">Submit</button>
     </form>
+  </div>
+
+  <!-- Confirmation Modal -->
+  <div id="confirmationModal" class="fixed z-10 inset-0 overflow-y-auto hidden">
+    <!-- Modal content... -->
+  </div>
+
+  <!-- Status Modal -->
+  <div id="statusModal" class="fixed z-10 inset-0 overflow-y-auto hidden">
+    <!-- Modal content... -->
   </div>
 </body>
 
