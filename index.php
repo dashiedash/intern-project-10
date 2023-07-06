163fe6ba371f6a8300a776255584d7bdca017386
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-06 11:31:20 +0800

    Add delete functionality

--- index.php
+++ index.php
@@ -2,11 +2,29 @@
 // index.php
 include 'connect.php';
 
+// Function to delete a record from the database
+function deleteRecord($id)
+{
+  global $pdo;
+  $query = 'DELETE FROM `stock-system` WHERE id = ?';
+  $stmt = $pdo->prepare($query);
+  $stmt->execute([$id]);
+}
+
 // Retrieve stock system data from the database
 $query = 'SELECT * FROM `stock-system`';
 $stmt = $pdo->prepare($query);
 $stmt->execute();
 $stockSystemData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Check if the delete button was clicked
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
+  $id = $_POST['delete'];
+  deleteRecord($id);
+  // Refresh the page after deletion
+  header("Location: index.php");
+  exit();
+}
 ?>
 
 
@@ -19,7 +37,6 @@ $stockSystemData = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Stock Management System</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 
 <body class="bg-gray-100">
@@ -49,7 +66,10 @@ $stockSystemData = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <td class="py-2 px-4 border-b"><?php echo $row['amount']; ?></td>
             <td class="py-2 px-4 border-b">
               <button class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Edit</button>
-              <button class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">Delete</button>
+              <form class="inline" method="POST" action="index.php">
+                <input type="hidden" name="delete" value="<?php echo $row['id']; ?>">
+                <button class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600" onclick="confirmDelete()">Delete</button>
+              </form>
             </td>
           </tr>
         <?php endforeach; ?>
@@ -57,5 +77,7 @@ $stockSystemData = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </table>
   </div>
 </body>
+
+<script src="script.js"></script>
 
 </html>
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function confirmSubmit() {
   return confirm("Are you sure you want to submit the form?");
 }
+
+function confirmDelete() {
+  return confirm("Are you sure you want to delete this record?");
+}
