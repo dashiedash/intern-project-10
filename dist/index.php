83a46cf20e5cbaa890675c5e4532770c12a0dd0f
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-07-07 11:02:01 +0800

    Clean up landing page code

--- dist/index.php
+++ dist/index.php
@@ -1,8 +1,6 @@
 <?php
-// index.php
 include 'connect.php';
 
-// Function to delete a record from the database
 function deleteRecord($id)
 {
   global $pdo;
@@ -11,23 +9,18 @@ function deleteRecord($id)
   $stmt->execute([$id]);
 }
 
-// Retrieve stock system data from the database
 $query = 'SELECT * FROM `stock-system`';
 $stmt = $pdo->prepare($query);
 $stmt->execute();
 $stockSystemData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Check if the delete button was clicked
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
   $id = $_POST['delete'];
   deleteRecord($id);
-  // Refresh the page after deletion
   header("Location: index.php");
   exit();
 }
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Stock Management System</title>
+  <title>Inventory Management System</title>
   <link rel="stylesheet" href="styles.css" type="text/css">
   <link href="output.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
@@ -48,7 +41,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
     <div class="my-5">
       <a href="create.php" class="rounded bg-cyan-800/70 py-2 px-4 text-white font-medium hover:bg-cyan-700/70">Create Item</a>
     </div>
-
 
     <div class="container mx-auto px-5 py-9 bg-[#eaedde] rounded-3xl">
       <table class="min-w-full my-3">
@@ -64,17 +56,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
         <tbody>
           <?php foreach ($stockSystemData as $row) : ?>
             <tr>
-              <td class="py-1 px-3 border-b border-cyan-700/70"><?php echo $row['id']; ?></td>
-              <td class="py-1 px-3 border-b border-cyan-700/70"><?php echo $row['item-name']; ?></td>
-              <td class="py-1 px-3 border-b border-cyan-700/70"><?php echo $row['brand']; ?></td>
-              <td class="py-1 px-3 border-b border-cyan-700/70"><?php echo $row['amount']; ?></td>
+              <td class="py-1 px-3 border-b border-cyan-700/70"><?= $row['id'] ?></td>
+              <td class="py-1 px-3 border-b border-cyan-700/70"><?= $row['item-name'] ?></td>
+              <td class="py-1 px-3 border-b border-cyan-700/70"><?= $row['brand'] ?></td>
+              <td class="py-1 px-3 border-b border-cyan-700/70"><?= $row['amount'] ?></td>
               <td class="py-1 px-3 border-b border-cyan-700/70">
                 <div class="flex justify-between content-center">
-                  <a href="update.php?id=<?php echo $row['id']; ?>" class="p-1 hover:text-cyan-700/70"><i class="fa-solid fa-pen-to-square"></i></a>
-
+                  <a href="update.php?id=<?= $row['id'] ?>" class="p-1 hover:text-cyan-700/70"><i class="fa-solid fa-pen-to-square"></i></a>
                   <form class="flex inline p-1" method="POST" action="index.php">
-                    <input type="hidden" name="delete" value="<?php echo $row['id']; ?>">
-                    <button onclick="return confirmAction('delete')" class="hover:text-pink-950/70"><i class=" fa-solid fa-trash"></i></button>
+                    <input type="hidden" name="delete" value="<?= $row['id'] ?>">
+                    <button onclick="return confirm('Are you sure you want to delete this record?')" class="hover:text-pink-950/70"><i class="fa-solid fa-trash"></i></button>
                   </form>
                 </div>
               </td>
@@ -84,9 +75,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
       </table>
     </div>
   </main>
-</body>
 
-<script src="script.js"></script>
-<script src="https://kit.fontawesome.com/a7437f466d.js" crossorigin="anonymous"></script>
+  <script src="script.js"></script>
+  <script src="https://kit.fontawesome.com/a7437f466d.js" crossorigin="anonymous"></script>
+</body>
 
 </html>
